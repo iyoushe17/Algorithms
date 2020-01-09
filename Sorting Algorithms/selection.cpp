4acd4374ce8571897f51ecce841811b0a579323a
Aayushi <aayushisingh3071@gmail.com>
2020-01-09 21:33:24 +0530

    added selection

--- Sorting Algorithms/selection.cpp
+++ Sorting Algorithms/selection.cpp
@@ -1,13 +1,13 @@
 /*************************************************************************************************************************
 * SELECTION SORT                                                                                                         *
 *                                                                                                                        *
-* ADAPTIVE: true                                                                                                         *
-* STABLE: true                                                                                                           *
-* Insertion sort is stable since it does not shift the element if it is equal.                                           *
-* COMPLEXITY: O(n²)
-* NUMBER OF COMPARISONS: 
+* ADAPTIVE: false. Even if list is sorted, the algorithm would take the same time.                                       *
+* STABLE: false, it does not preserve the order of elements                                                              *
+* COMPLEXITY: O(n²)                                                                                                      *
+* NUMBER OF COMPARISONS:                                                                                                 *
 * NUMBER OF SWAPS:                                                                                                       *
-* ASSUMPTION: For insertion sort, we assume that the first element is already in sorted order                            *
+* INTERMEDIATE STEP: Intermediate steps are useful as k steps would give k smallest elements.                            *
+* Selection sort is the only sorting technique with the least number of swaps                                            *
 *************************************************************************************************************************/
 
 #include <iostream>
@@ -16,7 +16,6 @@ void selection (int A[], int size) {
     for (int i = 0; i < size; i++) {
         int j = i+1, k = i, temp;
         while (j < size) {
-            //is j less than i
             if (A[j] < A[k])
                 k = j;
             j++;
@@ -30,9 +29,6 @@ void selection (int A[], int size) {
         std :: cout << A[k];
 
 }
-
-
-
 
 int main () {
     int size;
